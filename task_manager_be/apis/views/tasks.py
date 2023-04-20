9600596e2847a75e8c5ab5eb7f862457bddf23b7
pandasBrain <yash48237@gmail.com>
2023-04-19 23:41:10 -0600

    sorting and filtering

--- task_manager_be/apis/views/tasks.py
+++ task_manager_be/apis/views/tasks.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from dateutil.parser import parse
 
-from apis.models import User, Tasks
+from apis.models import Tasks
 from apis.serializers import TaskSerializer
 
 class TasksView(viewsets.ModelViewSet):
@@ -13,7 +13,25 @@ class TasksView(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         user_id = request.query_params.get("user_id", "")
         user_id = user_id.strip()
-        user_tasks = self.queryset.filter(user_id=user_id).order_by("-due_date")
+        
+        sort_title = request.query_params.get("sort_title", "")
+        sort_date = request.query_params.get("sort_date", "asc")
+        status_filter = request.query_params.getlist("status_filter", [])
+        user_tasks = self.queryset.filter(user_id=user_id)
+        user_tasks_sorted = None
+        if status_filter:
+            user_tasks = user_tasks.filter(status__in=status_filter)
+        if sort_title=="asc":
+            user_tasks_sorted = user_tasks.order_by("title")
+        if sort_title=="desc":
+            user_tasks_sorted = user_tasks.order_by("-title")
+        if sort_date=="asc":
+            user_tasks_sorted = user_tasks.order_by("due_date")
+        if sort_title=="desc":
+            user_tasks_sorted = user_tasks.order_by("-due_date")
+        if user_tasks_sorted:
+            user_tasks = user_tasks_sorted
+        # user_tasks = self.queryset.filter(user_id=user_id).order_by("due_date")
         data = TaskSerializer(user_tasks, many=True).data
         return Response({"success": True, "data": data})
 
